8532213deac72aa3fa8a44ff24b8bfa0a752d64f
Jonas <sturmente@proton.me>
2023-08-31 20:17:21 +0200

    Check if all needed env variables are set

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+require("dotenv/config")();
+
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const WEBHOOK = process.env.WEBHOOK;
+
+if (!USERNAME) {
+	console.error("No username");
+	process.exit();
+}
+
+if (!PASSWORD) {
+	console.error("No password");
+	process.exit();
+}
+
+if (!WEBHOOK) {
+	console.error("No webhook");
+	process.exit();
+}
