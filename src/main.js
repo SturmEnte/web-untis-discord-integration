f044c8ff343decf2c56fbfc77a6a3b20af8486c8
Jonas <sturmente@proton.me>
2023-08-31 21:44:07 +0200

    Finished sendSubstitutionPlan function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 require("dotenv/config");
 const puppeteer = require("puppeteer");
-const axios = require("axios");
+const fetch = require("node-fetch").default;
+const formData = require("form-data");
+const fs = require("fs");
+const path = require("path");
 
 const USERNAME = process.env.UNTIS_USERNAME;
 const PASSWORD = process.env.UNTIS_PASSWORD;
@@ -33,15 +36,11 @@ if (!WEBHOOK) {
 	process.exit();
 }
 
-(async () => {
+async function sendSubstitutionPlan() {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
 	await page.goto(SCHOOL_LOGIN_URL);
-
-	//await page.setViewport({ width: 2200, height: 800 });
-
-	await page.screenshot({ path: "./test/test1.png" });
 
 	await page.type('[type="text"]', USERNAME);
 	await page.type('[type="password"]', PASSWORD);
@@ -52,13 +51,42 @@ if (!WEBHOOK) {
 	setTimeout(async () => {
 		await page.goto(`https://borys.webuntis.com/WebUntis/api/public/printpreview/timetable?type=1&id=${TABLE_ID}&date=${date.yyyymmdd()}&formatId=1`);
 
-		await page.screenshot({ path: "./test/test3.png", captureBeyondViewport: true, fullPage: true });
+		if (!fs.existsSync(path.join(__dirname, "../cache"))) fs.mkdirSync(path.join(__dirname, "../cache"));
+
+		await page.screenshot({ path: "./cache/subplan.png", captureBeyondViewport: true, fullPage: true });
+
+		const form = new formData();
+		form.append("subplan.png", fs.createReadStream(path.join(__dirname, "../cache/subplan.png")));
+		form.append("payload_json", JSON.stringify({ content: `**${date.ddmmyyyy()} ${date.hhmm()}**` }));
+
+		fetch(WEBHOOK, { method: "POST", headers: form.getHeaders(), body: form }).then((res) => {
+			if (res.status == 200) {
+				console.log("success");
+				return;
+			}
+
+			res.json().then(console.log);
+		});
 	}, 5000);
-})();
+}
 
 Date.prototype.yyyymmdd = function () {
-	var mm = this.getMonth() + 1; // getMonth() is zero-based
+	var mm = this.getMonth() + 1;
 	var dd = this.getDate();
 
 	return [this.getFullYear(), (mm > 9 ? "" : "0") + mm, (dd > 9 ? "" : "0") + dd].join("");
+};
+
+Date.prototype.ddmmyyyy = function () {
+	var mm = this.getMonth() + 1;
+	var dd = this.getDate();
+
+	return [(dd > 9 ? "" : "0") + dd, (mm > 9 ? "" : "0") + mm, this.getFullYear()].join(".");
+};
+
+Date.prototype.hhmm = function () {
+	var mm = this.getMinutes();
+	var hh = this.getHours();
+
+	return [(hh > 9 ? "" : "0") + hh, (mm > 9 ? "" : "0") + mm].join(":");
 };
