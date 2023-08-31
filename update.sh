5b1a81685b5368645bfc05efa0565b175e2a1da3
Jonas <sturmente@proton.me>
2023-08-31 22:08:28 +0200

    Added build files

---
+++ update.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "\033[104mStarting update\033[0m"
+echo "\033[104mPull from GitHub\033[0m"
+git pull
+echo "\033[104mInstall npm packages\033[0m"
+npm i
+echo "\033[104mBuild Docker image\033[0m"
+docker build -t duck-bot .
+echo "\033[104mUpdated\033[0m"
