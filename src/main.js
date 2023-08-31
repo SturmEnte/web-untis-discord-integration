f0be67ada52657c7a1b2c5967541140e3e9d5bad
Jonas <sturmente@proton.me>
2023-08-31 21:04:34 +0200

    Make screenshot of substitution plan

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-require("dotenv/config")();
+require("dotenv/config");
+const puppeteer = require("puppeteer");
+const axios = require("axios");
 
-const USERNAME = process.env.USERNAME;
-const PASSWORD = process.env.PASSWORD;
+const USERNAME = process.env.UNTIS_USERNAME;
+const PASSWORD = process.env.UNTIS_PASSWORD;
+const TABLE_ID = process.env.UNTIS_TABLEID;
+const SCHOOL_LOGIN_URL = process.env.SCHOOL_LOGIN_URL;
 const WEBHOOK = process.env.WEBHOOK;
 
 if (!USERNAME) {
@@ -14,7 +18,47 @@ if (!PASSWORD) {
 	process.exit();
 }
 
+if (!TABLE_ID) {
+	console.error("No table id");
+	process.exit();
+}
+
+if (!SCHOOL_LOGIN_URL) {
+	console.error("No school login url");
+	process.exit();
+}
+
 if (!WEBHOOK) {
 	console.error("No webhook");
 	process.exit();
 }
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto(SCHOOL_LOGIN_URL);
+
+	await page.setViewport({ width: 2200, height: 800 });
+
+	await page.screenshot({ path: "./test/test1.png" });
+
+	await page.type('[type="text"]', USERNAME);
+	await page.type('[type="password"]', PASSWORD);
+	await page.keyboard.press("Enter");
+
+	const date = new Date();
+
+	setTimeout(async () => {
+		await page.goto(`https://borys.webuntis.com/WebUntis/api/public/printpreview/timetable?type=1&id=${TABLE_ID}&date=${date.yyyymmdd()}&formatId=1`);
+
+		await page.screenshot({ path: "./test/test3.png" });
+	}, 5000);
+})();
+
+Date.prototype.yyyymmdd = function () {
+	var mm = this.getMonth() + 1; // getMonth() is zero-based
+	var dd = this.getDate();
+
+	return [this.getFullYear(), (mm > 9 ? "" : "0") + mm, (dd > 9 ? "" : "0") + dd].join("");
+};
